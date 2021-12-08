ede0347b858c39eef399291106e5acb448d14abc
alex-gry <alexiane.guerry@epfl.ch>
2021-12-08 11:20:47 +0100

    change of sgd in glove_template, data exploration in run_file, trying training models, glove manipulation

--- glove_template.py
+++ glove_template.py
@@ -4,7 +4,6 @@ import numpy as np
 import pickle
 import random
 
-#DATA_PATH = "../twitter-datasets/"
 
 def main():
     print("loading cooccurrence matrix")
@@ -17,7 +16,6 @@ def main():
 
     print("initializing embeddings")
     embedding_dim = 20
-    #weights:
     xs = np.random.normal(size=(cooc.shape[0], embedding_dim))
     ys = np.random.normal(size=(cooc.shape[1], embedding_dim))
 
@@ -25,35 +23,26 @@ def main():
     alpha = 3 / 4
 
     epochs = 10
-    #loss_per_epoch=[]
 
     for epoch in range(epochs):
         print("epoch {}".format(epoch))
         for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
-        
-        # fill in your SGD code here, 
-        # for the update resulting from co-occurence (i,j)
-        
-            #Computing grad_xi and grad_yj
-            e_ij = np.dot(xs[ix, :], ys[jy, :]) - np.log(n)
-            #weighting function defined by the GloVe authors:
-            fM_ij = min(1.0, (n / nmax) ** alpha)
-            grad_xi = 2 * fM_ij * e_ij * ys[jy, :]
-            grad_yj = 2 * fM_ij * e_ij * xs[ix, :]
 
-            #Computing intermediate loss for ix and jy:
-            #L += fM_ij * e_ij**2
-
-            #Updating the weights
-            xs[ix, :] -= eta * grad_xi
-            ys[jy, :] -= eta * grad_yj
-        
-        #loss_per_epoch.append(L)
-
+            # fill in your SGD code here, 
+            # for the update resulting from co-occurence (i,j)
+            logn = np.log(n)
+            fn = min(1.0, (n / nmax) ** alpha)
+            x, y = xs[ix, :], ys[jy, :]
+            scale = 2 * eta * fn * (logn - np.dot(x, y))
+            xs[ix, :] += scale * y
+            ys[jy, :] += scale * x
+            
+            
     np.save('embeddings', xs)
-    #np.save('embeddings_full_22epoch_{}dim'.format(embedding_dim), xs)
-    #np.save('loss_22epoch_{}dim'.format(embedding_dim), loss_per_epoch)
 
 
 if __name__ == '__main__':
     main()
+
+    
+    
